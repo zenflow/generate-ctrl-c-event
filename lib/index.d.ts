4d00fd3491fe5ba3b3ec97c45f858fe613186fb3
Matthew Francis Brunetti <zenflow87@gmail.com>
2021-03-22 08:44:08 -0400

    Add typescript type definitions, closes #3

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+export declare function generateCtrlC(): boolean;
+export declare function generateCtrlCAsync(): Promise<boolean>;
+export declare function generateCtrlBreak(dwProcessGroupId: number): boolean;
+export declare function generateCtrlBreakAsync(dwProcessGroupId: number): Promise<boolean>;
