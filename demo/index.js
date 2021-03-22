f700b84e014fa900aea8a03b5b438dcbad372422
Matthew Francis Brunetti <zenflow87@gmail.com>
2021-03-22 07:57:06 -0400

    Improve demo script with default arguments

--- demo/index.js
+++ demo/index.js
@@ -2,7 +2,7 @@ const { demoRoutine } = require("./demoRoutine");
 const lib = require("..");
 
 const args = process.argv.slice(2);
-const functionName = args[0];
-const functionArg = args[1] && Number(args[1]);
+const functionName = args[0] ? args[0] : "generateCtrlC";
+const functionArg = args[1] ? Number(args[1]) : 0;
 
 demoRoutine(() => lib[functionName](functionArg));
